9dbdc2fda5e6d49a96b8117318c881b1c58e33fc
Radhey Kalra <radheykalra901@gmail.com>
2023-10-08 21:00:59 +0530

    .

--- Activity 2/area_peremeter.c
+++ Activity 2/area_peremeter.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 #define PI 3.14159
-int main() {
-    float radius, side, length, width;
-    float circle_area, circle_perimeter, square_area, square_perimeter, rectangle_area, rectangle_perimeter;
-
+void main() {
+    float r, s, l, w;
+    float ca, cp, sa, sp, ra, rp;
     // Calculate area and perimeter of circle
     printf("Enter the radius of the circle: ");
-    scanf("%f", &radius);
-    circle_area = PI * radius * radius;
-    circle_perimeter = 2 * PI * radius;
-
+    scanf("%f", &r);
+    ca = PI * r * r;
+    cp = 2 * PI * r;
     // Calculate area and perimeter of square
     printf("Enter the side of the square: ");
-    scanf("%f", &side);
-    square_area = side * side;
-    square_perimeter = 4 * side;
-
+    scanf("%f", &s);
+    sa = s * s;
+    sp = 4 * s;
     // Calculate area and perimeter of rectangle
     printf("Enter the length and width of the rectangle: ");
-    scanf("%f %f", &length, &width);
-    rectangle_area = length * width;
-    rectangle_perimeter = 2 * (length + width);
-
+    scanf("%f %f", &l, &w);
+    ra = l * w;
+    rp = 2 * (l + w);
     // Print the results
-    printf("\nCircle:\nArea = %.2f\nPerimeter = %.2f\n", circle_area, circle_perimeter);
-    printf("\nSquare:\nArea = %.2f\nPerimeter = %.2f\n", square_area, square_perimeter);
-    printf("\nRectangle:\nArea = %.2f\nPerimeter = %.2f\n", rectangle_area, rectangle_perimeter);
-
-    return 0;
+    printf("\nCircle:\nArea = %.2f\nPerimeter = %.2f\n", ca, cp);
+    printf("\nSquare:\nArea = %.2f\nPerimeter = %.2f\n", sa, sp);
+    printf("\nRectangle:\nArea = %.2f\nPerimeter = %.2f\n", ra, rp);
 }
