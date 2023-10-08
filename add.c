4c95a1f2a03c946eba780ad82151dfae1f4fdf64
Radhey Kalra <radheykalra901@gmail.com>
2023-10-08 17:34:53 +0530

    kush be type krdo

---
+++ add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    printf("enrer two number to be added: ");
+    scanf("%d %d", &a, &b);
+    c = a + b;  
+    printf("The sum of %d and %d is %d :", a, b, c);
+    return 0;  
+
+}
